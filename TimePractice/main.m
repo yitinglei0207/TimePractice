9e1b01c25a55d4d88dbd7aef68e9b944f27165b0
yitinglei <yitinglei0207@gmail.com>
2015-04-30 15:06:56 +0800

    Initial Commit

---
+++ TimePractice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TimePractice
+//
+//  Created by Jason Lei on 2015/4/30.
+//  Copyright (c) 2015年 AlphaCamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
